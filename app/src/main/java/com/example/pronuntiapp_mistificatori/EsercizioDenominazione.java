b1174cfa09c712ac1b261a877731bd662b891926
mSomma2 <m.somma19@studenti.uniba.it>
2024-03-23 17:42:06 +0100

    Finite le correzioni, speriamo che pek si muove e abbiamo finito

--- app/src/main/java/com/example/pronuntiapp_mistificatori/EsercizioDenominazione.java
+++ app/src/main/java/com/example/pronuntiapp_mistificatori/EsercizioDenominazione.java
@@ -213,6 +213,10 @@ public class EsercizioDenominazione extends AppCompatActivity{
         finish();    }
 
     public void checkResul(View view) {
+        if(isRecording){
+            rec(view);
+        }
+
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
 
--- app/src/main/java/com/example/pronuntiapp_mistificatori/EsercizioRipetizione.java
+++ app/src/main/java/com/example/pronuntiapp_mistificatori/EsercizioRipetizione.java
@@ -110,6 +110,9 @@ public class EsercizioRipetizione extends AppCompatActivity {
     }
 
     public void checkResul(View view) {
+        if(isRecording){
+            rec(view);
+        }
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
 
